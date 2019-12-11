5426097bfb453e24fa572d81c56d61c48e5a2699
yangxin <1393003255@qq.com>
2019-12-11 14:10:07 +0800

    feat: 单节点Eureka Server的开发

---
+++ ad-eureka/src/main/java/org/yangxin/ad/EurekaApplication.java
@@ -0,0 +1,19 @@
+package org.yangxin.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务发现与注册
+ *
+ * @author yangxin
+ * 2019/12/11 14:02
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnableEurekaServer.class, args);
+    }
+}
