576a04738653d20bbd5806dce4337ad214657f14
yangxin <1393003255@qq.com>
2020-01-03 23:39:27 +0800

    feat: 统一配置的开发

---
+++ ad-service/ad-common/src/main/java/org/yangxin/ad/config/WebConfiguration.java
@@ -0,0 +1,21 @@
+package org.yangxin.ad.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author yangxin
+ * 2020/01/03 23:37
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2CborHttpMessageConverter());
+    }
+}
