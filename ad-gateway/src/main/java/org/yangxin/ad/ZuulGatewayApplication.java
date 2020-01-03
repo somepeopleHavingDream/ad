cdbff668a79a4a373656f38a9c6efaf176acf732
yangxin <1393003255@qq.com>
2020-01-03 22:23:17 +0800

    feat: 网关启动程序的开发

---
+++ ad-gateway/src/main/java/org/yangxin/ad/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package org.yangxin.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关
+ *
+ * @author yangxin
+ * 2020/01/03 10:17
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
