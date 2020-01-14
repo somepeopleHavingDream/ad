20541349aadcc5fb17183357d21a1690746a239d
yangxin <1393003255@qq.com>
2020-01-14 16:09:18 +0800

    feat: 【搭建微服务】创建广告检索系统子模块

---
+++ ad-service/ad-search/src/main/java/org/yangxin/ad/SearchApplication.java
@@ -0,0 +1,29 @@
+package org.yangxin.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 检索模块
+ *
+ * @author yangxin
+ * 2020/01/14 16:04
+ */
+@EnableFeignClients
+@EnableEurekaClient
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
