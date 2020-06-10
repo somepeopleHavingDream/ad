45c2b7a4f346ca5c771c9b0d3b162fb6e9e9a90f
yangxin <1393003255@qq.com>
2020-06-10 20:43:36 +0800

    feat: 第三层级索引操作的实现

--- ad-service/ad-search/src/main/java/org/yangxin/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/org/yangxin/ad/handler/AdLevelDataHandler.java
@@ -1,15 +1,23 @@
 package org.yangxin.ad.handler;
 
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.yangxin.ad.dump.table.AdCreativeTable;
+import org.yangxin.ad.dump.table.AdCreativeUnitTable;
 import org.yangxin.ad.dump.table.AdPlanTable;
+import org.yangxin.ad.dump.table.AdUnitTable;
 import org.yangxin.ad.index.DataTable;
 import org.yangxin.ad.index.IndexAware;
 import org.yangxin.ad.index.adplan.AdPlanIndex;
 import org.yangxin.ad.index.adplan.AdPlanObject;
+import org.yangxin.ad.index.adunit.AdUnitIndex;
+import org.yangxin.ad.index.adunit.AdUnitObject;
 import org.yangxin.ad.index.creative.CreativeIndex;
 import org.yangxin.ad.index.creative.CreativeObject;
+import org.yangxin.ad.index.creativeunit.CreativeUnitIndex;
+import org.yangxin.ad.index.creativeunit.CreativeUnitObject;
 import org.yangxin.ad.mysql.constant.OpType;
+import org.yangxin.ad.utils.CommonUtils;
 
 /**
  * 1. 索引之间存在着层级的划分，也就是依赖关系的划分
@@ -40,6 +48,44 @@ public class AdLevelDataHandler {
                 adCreativeTable.getAuditStatus(),
                 adCreativeTable.getAdUrl());
         handleBinlogEvent(DataTable.of(CreativeIndex.class), creativeObject.getAdId(), creativeObject, opType);
+    }
+
+    public static void handleLevel3(AdUnitTable adUnitTable, OpType opType) {
+        AdPlanObject adPlanObject = DataTable.of(AdPlanIndex.class).get(adUnitTable.getPlanId());
+        if (adPlanObject == null) {
+            log.error("handleLevel3 found AdPlanObject error: [{}]", adUnitTable.getPlanId());
+            return;
+        }
+
+        AdUnitObject adUnitObject = new AdUnitObject(adUnitTable.getUnitId(),
+                adUnitTable.getUnitStatus(),
+                adUnitTable.getPositionType(),
+                adUnitTable.getPlanId(),
+                adPlanObject);
+
+        handleBinlogEvent(DataTable.of(AdUnitIndex.class), adUnitTable.getUnitId(), adUnitObject, opType);
+    }
+
+    public static void handleLevel3(AdCreativeUnitTable adCreativeUnitTable, OpType opType) {
+        if (OpType.UPDATE == opType) {
+            log.error("AdCreativeUnitIndex not support update");
+            return;
+        }
+
+        AdUnitObject adUnitObject = DataTable.of(AdUnitIndex.class).get(adCreativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(CreativeIndex.class).get(adCreativeUnitTable.getAdId());
+        if (adUnitObject == null || creativeObject == null) {
+            log.error("AdCreativeUnitTable index error: [{}]", JSON.toJSON(adCreativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(adCreativeUnitTable.getAdId(),
+                adCreativeUnitTable.getUnitId());
+        handleBinlogEvent(DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()),
+                creativeUnitObject,
+                opType);
     }
 
     private static <K, V> void handleBinlogEvent(IndexAware<K, V> index, K key, V value, OpType opType) {
--- ad-service/ad-search/src/main/java/org/yangxin/ad/utils/CommonUtils.java
+++ ad-service/ad-search/src/main/java/org/yangxin/ad/utils/CommonUtils.java
@@ -14,4 +14,14 @@ public class CommonUtils {
     public static <K, V> V getOrCreate(K key, Map<K, V> map, Supplier<V> factory) {
         return map.computeIfAbsent(key, k -> factory.get());
     }
+
+    public static String stringConcat(String... args) {
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg).append("-");
+        }
+        result.deleteCharAt(result.length() - 1);
+
+        return result.toString();
+    }
 }
