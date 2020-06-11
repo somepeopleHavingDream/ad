0c3842143abd078d389267001d6f8d195dd2a170
yangxin <1393003255@qq.com>
2020-06-11 21:19:45 +0800

    feat: 全量索引加载的实现（2）

--- ad-service/ad-search/src/main/java/org/yangxin/ad/index/IndexFileLoader.java
+++ ad-service/ad-search/src/main/java/org/yangxin/ad/index/IndexFileLoader.java
@@ -4,10 +4,7 @@ import com.alibaba.fastjson.JSON;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 import org.yangxin.ad.dump.DumpConstant;
-import org.yangxin.ad.dump.table.AdCreativeTable;
-import org.yangxin.ad.dump.table.AdCreativeUnitTable;
-import org.yangxin.ad.dump.table.AdPlanTable;
-import org.yangxin.ad.dump.table.AdUnitTable;
+import org.yangxin.ad.dump.table.*;
 import org.yangxin.ad.handler.AdLevelDataHandler;
 import org.yangxin.ad.mysql.constant.OpType;
 
@@ -48,6 +45,23 @@ public class IndexFileLoader {
                 DumpConstant.AD_CREATIVE_UNIT));
         adCreativeUnitStringList.forEach(cu
                 -> AdLevelDataHandler.handleLevel3(JSON.parseObject(cu, AdCreativeUnitTable.class),
+                OpType.ADD));
+
+        List<String> adUnitDistrictStringList = loadDumpData(String.format("%s%s", DumpConstant.DATA_ROOT_DIR,
+                DumpConstant.AD_UNIT_DISTRICT));
+        adUnitDistrictStringList.forEach(d
+                -> AdLevelDataHandler.handleLevel4(JSON.parseObject(d, AdUnitDistrictTable.class),
+                OpType.ADD));
+
+        List<String> adUnitItStringList = loadDumpData(String.format("%s%s", DumpConstant.DATA_ROOT_DIR,
+                DumpConstant.AD_UNIT_IT));
+        adUnitItStringList.forEach(i -> AdLevelDataHandler.handleLevel4(JSON.parseObject(i, AdUnitItTable.class),
+                OpType.ADD));
+
+        List<String> adUnitKeywordStringList = loadDumpData(String.format("%s%s", DumpConstant.DATA_ROOT_DIR,
+                DumpConstant.AD_UNIT_KEYWORD));
+        adUnitKeywordStringList.forEach(k
+                -> AdLevelDataHandler.handleLevel4(JSON.parseObject(k, AdUnitKeywordTable.class),
                 OpType.ADD));
     }
 
