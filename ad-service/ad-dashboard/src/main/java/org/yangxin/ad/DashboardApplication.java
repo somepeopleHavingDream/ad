a79aae560893ac3b8f0aea620eb78cb6ab79e2bd
yangxin <1393003255@qq.com>
2020-08-25 15:55:15 +0800

    feat: Dashboard模块的实现

---
+++ ad-service/ad-dashboard/src/main/java/org/yangxin/ad/DashboardApplication.java
@@ -0,0 +1,20 @@
+package org.yangxin.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author yangxin
+ * 2020/08/25 15:54
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
